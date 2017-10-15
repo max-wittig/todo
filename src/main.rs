a56791212c11fccb09747e4b5a875dcf27d5f6dd
Max Wittig <max.wittig95@gmail.com>
2017-10-15 16:58:14 +0200

    feat: add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
+#[macro_use]
+extern crate clap;
+
 use std::str;
+use clap::{Arg, App, ArgGroup};
+
+fn get_args() {
+    let mut task_name :String = String::from("sfsdf");
+    let mut task_description : String;
+
+    let matches = App::new("Simple Todo")
+        .version("1.0.0")
+        .arg(Arg::with_name("task")
+            .short("t")
+            .long("task")
+            .help("The name of the task")
+            .takes_value(true))
+        .arg(Arg::with_name("description")
+            .short("d")
+            .long("description")
+            .help("Description")
+            .default_value("")
+            .takes_value(true))
+        .get_matches();
+    match matches.occurrences_of("task") {
+        0 => println!("Please specify a task to add"),
+        1 => task_name = value_t_or_exit!(matches.value_of("task"), String),
+        _ => println!("You can only use this argument once"),
+    };
+    match matches.occurrences_of("description") {
+        0 => println!("test"),
+        1 => task_description = value_t_or_exit!(matches.value_of("description"), String),
+        _ => println!("You can only use this argument once"),
+    };
+
+    println!("{}", task_name);
+}
 
 fn main() {
     let mut task_list = Vec::new();
+    get_args();
     let task_name = String::from("Test");
     let task_description = String::from("Test description");
     todo::add_task(&mut task_list, task_name, task_description);
@@ -29,7 +66,7 @@ pub mod todo {
 
     impl Task {
         fn to_json(&self) -> String {
-
+            String::from("whatever")
         }
     }
 }
