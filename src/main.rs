373a0982d2f18290b7070672813a5de4b84daaac
Max Wittig <max.wittig95@gmail.com>
2017-10-26 21:56:18 +0200

    refactor: modulize todo

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
-extern crate clap;
 extern crate serde_yaml;
+extern crate clap;
+
+mod todo;
 
 use std::str;
-use clap::{Arg, App, ArgGroup};
+use clap::{Arg, App};
+use std::process::exit;
 
-fn get_args() -> (String, String) {
-    let mut task_name :String = String::from("");
-    let mut task_description : String = String::from("");
-
+fn get_args() -> (String, String, bool) {
     let matches = App::new("Simple Todo")
         .version("1.0.0")
         .arg(Arg::with_name("task")
@@ -22,94 +21,53 @@ fn get_args() -> (String, String) {
             .short("d")
             .long("description")
             .help("Description")
+            .multiple(true)
             .default_value("")
             .takes_value(true))
+        .arg(Arg::with_name("mark_done")
+            .short("md")
+            .long("mark_done")
+            .help("Mark task as done")
+            .takes_value(false))
         .get_matches();
-    match matches.occurrences_of("task") {
-        0 => println!("Please specify a task to add"),
-        1 => task_name = value_t_or_exit!(matches.value_of("task"), String),
-        _ => println!("You can only use this argument once"),
-    };
-    match matches.occurrences_of("description") {
-        0 => println!("test"),
-        1 => task_description = value_t_or_exit!(matches.value_of("description"), String),
-        _ => println!("You can only use this argument once"),
-    };
 
-    (task_name, task_description)
+    let task_name = matches.value_of("task");
+    let task_description = matches.value_of("description");
+    let mark_done;
+    match matches.occurrences_of("mark_done") {
+        1 => mark_done = true,
+        _ => mark_done = false
+    }
+
+    if task_name.is_some() {
+        let description;
+        if task_description.is_some() {
+            description = task_description.unwrap().to_string();
+        }
+        else {
+            description = String::new();
+        }
+
+        (task_name.unwrap().to_string(), description, mark_done)
+    }
+    else {
+        println!("Please set a task name!");
+        exit(1);
+    }
+
 }
 
 fn main() {
     let mut todo_list = todo::load();
-    let (task_name, task_description) = get_args();
-    todo::TodoList::add_task(&mut todo_list, task_name, task_description);
+    let (task_name, task_description, mark_done) = get_args();
+    if todo_list.task_exists(&task_name) {
+        if mark_done {
+            todo_list.mark_done(task_name);
+        }
+    }
+    else {
+        todo::TodoList::add_task(&mut todo_list, task_name, task_description, mark_done);
+    }
+
     todo_list.save(String::from("todo.yaml"));
 }
-
-pub mod todo {
-    use std::fs::File;
-    use serde_yaml;
-    use std::io::Read;
-    use std::io::Error;
-    use std::io::Write;
-
-    pub fn load() -> TodoList {
-        match File::open("todo.yaml") {
-            Err(_) => {
-                println!("File not found. Creating new todo list...");
-                new()
-            },
-            Ok(mut file) => {
-                let mut file_content = String::new();
-                file.read_to_string(&mut file_content).unwrap();
-                serde_yaml::from_str(&mut file_content).unwrap()
-            },
-        }
-    }
-
-    pub fn new() -> TodoList {
-        TodoList {
-            task_list: Vec::new()
-        }
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct TodoList {
-        task_list : Vec<Task>
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Task {
-        task_name: String,
-        description: String,
-        done : bool,
-    }
-
-    impl TodoList {
-        pub fn add_task(&mut self, task_name : String, description : String) {
-            let task = Task {
-                task_name,
-                description,
-                done: false,
-            };
-
-            self.task_list.push(task);
-        }
-
-        pub fn save(&self, filename : String) {
-            let mut file = match File::create(&filename) {
-                Err(why) => panic!("Could not create file {0}", &filename),
-                Ok(file) => file,
-            };
-            file.write_all(serde_yaml::to_string(self).unwrap().as_bytes())
-                .expect("File could not be written");
-        }
-    }
-
-    impl Task {
-        fn save(&self) -> String {
-            serde_yaml::to_string(self).unwrap()
-        }
-    }
-}
-
---
+++ src/todo.rs
@@ -0,0 +1,84 @@
+use std::fs::File;
+use serde_yaml;
+use std::io::Read;
+use std::io::Write;
+use std::process::exit;
+
+pub fn load() -> TodoList {
+    match File::open("todo.yaml") {
+        Err(_) => {
+            println!("File not found. Creating new todo list...");
+            new()
+        },
+        Ok(mut file) => {
+            let mut file_content = String::new();
+            file.read_to_string(&mut file_content).unwrap();
+            serde_yaml::from_str(&mut file_content).unwrap()
+        },
+    }
+}
+
+pub fn new() -> TodoList {
+    TodoList {
+        task_list: Vec::new()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TodoList {
+    task_list : Vec<Task>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Task {
+    task_name: String,
+    description: String,
+    done : bool,
+}
+
+impl Task {
+    pub fn mark_down(&mut self) {
+        self.done = true;
+    }
+}
+
+impl TodoList {
+    pub fn add_task(&mut self, task_name : String, description : String, done : bool) {
+        let task = Task {
+            task_name,
+            description,
+            done,
+        };
+
+        self.task_list.push(task);
+    }
+
+    pub fn mark_done(&mut self, task_name : String) {
+        for current_task in self.task_list.iter_mut() {
+            if current_task.task_name == task_name {
+                current_task.mark_down()
+            }
+        }
+    }
+
+    pub fn task_exists(self, task_name : &String) -> bool {
+        for current_task in self.task_list.iter() {
+            if current_task.task_name == task_name.as_str() {
+                return true
+            }
+        }
+        false
+    }
+
+    pub fn save(&self, filename : String) {
+        let mut file = match File::create(&filename) {
+            Err(_) => {
+                println!("Could not create file");
+                exit(1);
+            }
+            Ok(file) => file,
+        };
+        file.write_all(serde_yaml::to_string(self).unwrap().as_bytes())
+            .expect("File could not be written");
+    }
+}
